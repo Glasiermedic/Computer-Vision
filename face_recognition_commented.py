04cad50f902c5528f06dfeafd5342ac4848e6b9d
Glasiermedic <wrolfson@glasierstudios.com>
2020-05-18 09:35:49 -0700

    Update face_recognition_commented.py
    added in smile detection and improved accuracy for eyes, fine tuned smile detection

--- face_recognition_commented.py
+++ face_recognition_commented.py
@@ -1,11 +1,12 @@
 # This code has extra comments to help explain the process and is updated to reflect changes in OpenCv and python
 
-# make sure you download the xml files and install the latest opencv.
+# make sure you download the xml files and place in same directory as this file and install the latest opencv.
 
 import cv2
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
 
 def detect(gray, frme): # function that accepts a greyscale and the original frame
     faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.3, minNeighbors = 5)
@@ -14,11 +15,14 @@ def detect(gray, frme): # function that accepts a greyscale and the original fra
     for (x,y,w,h) in faces: 
         cv2.rectangle(frme, (x,y), (x+w, y+h), (255,0,0), 2) # We pain a rectangle around the face
         rt_gray = gray[y:y+h, x:x+w] # suspect facial region of grayscale image
-        rt_color = frame[y:y+h, x:x+w]# applying the suspect facial region to the original image
-        eyes = eye_cascade.detectMultiScale(rt_gray, 1.1, 3) # now applying multiscale inside suspect region 
+        rt_color = frme[y:y+h, x:x+w]# applying the suspect facial region to the original image
+        eyes = eye_cascade.detectMultiScale(rt_gray, 1.2, 15) # now applying multiscale inside suspect region 
         # to look for the eyes
         for (ex, ey, ew, eh) in eyes: # applied to each eye detected 
             cv2.rectangle(rt_color, (ex, ey), (ex+ew, ey+eh), (0,255, 0), 2) # put rectangle around suspect eyes in suspect face
+        smile = smile_cascade.detectMultiScale(rt_gray, scaleFactor = 1.7,minNeighbors =12)
+        for (sx, sy, sw, sh) in smile: 
+            cv2.rectangle(rt_color, (sx,sy), (sx+sw, sy+sh), (255,0,255),2)
     return frme
 
 video_capture = cv2.VideoCapture(0) # turning on the video source if set to '0' or the file if location provided
